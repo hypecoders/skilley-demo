3f055645de5ce974c165f37b3b37994f34fe28b6
Kubo <duris@inqool.cz>
2021-12-01 11:10:15 +0100

    crlf changed, added AppBar

---
+++ src/components/global-navbar/navItems.ts
@@ -0,0 +1,22 @@
+import { NavItem } from '../../common/type';
+
+const NAV_ITEMS: Array<NavItem> = [
+	{
+		label: 'Home',
+		href: '/'
+	},
+	{
+		label: 'Pool',
+		href: '/pool'
+	},
+	{
+		label: 'Pricing',
+		href: '/pricing'
+	},
+	{
+		label: 'Invoices',
+		href: '/invoices'
+	}
+];
+
+export default NAV_ITEMS;
