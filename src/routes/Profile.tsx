a7fdf87cbd3834bceb3ee64e0bff47a8ac7b13ec
Kubo <duris@inqool.cz>
2021-12-06 20:53:18 +0100

    Changed FlexHeight to 100vh at Profile

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -43,7 +43,7 @@ const Profile = () => {
 	}
 
 	return (
-		<Flex justifyContent="center" bg="white">
+		<Flex height="100vh" justifyContent="center" bg="white">
 			<Flex align="center" flexDir="column" width="50%">
 				<Heading my={10} lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
 					User Profile
